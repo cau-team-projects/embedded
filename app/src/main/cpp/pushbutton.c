ef787bcaafe5bad823734256ca64c41de83901b5
Lee Jae Hyeon <euphoric_n@naver.com>
2020-11-23 20:13:35 +0900

    pushbutton work in progress

--- app/src/main/cpp/pushbutton.c
+++ app/src/main/cpp/pushbutton.c
@@ -8,29 +8,63 @@
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <android/log.h>
-#include <sys/types.h>
-#include <stdlib.h>
 
-#define UDELAY 10000
+#define UDELAY 1000000
 
-/*
+int translate(int value) {
+    unsigned char bytevalues[9];
+    int cnt = 0;
+    int last = -1;
+    for(int i=0; i<9; i++) {
+        bytevalues[i] = (value >> 2*i) % 2;
+        cnt = bytevalues[i] ? cnt + 1 : cnt;
+        last = bytevalues[i] ? i : last;
+    }
+    if(cnt == 0) {
+        return 0;
+    } else if (cnt == 1) {
+        return last;
+    }
+}
+
 JNIEXPORT jint JNICALL
 Java_org_blockinger_game_activities_GameActivity_setInput(JNIEnv *env, jobject thiz) {
-    int fd;
+    int fd, flag;
     unsigned char ret;
-    pid_t pid;
+    int value = 0;
 
     fd = open("/dev/pushbutton", O_RDWR);
     if(fd < 0) {
         printf("Error opening device\n");
     }
 
-    switch(pid = fork()) {
-        case -1 :
-            // fork failed
-
+    while(1) {
+        read(fd, &value, sizeof(int));
+        flag = translate(value);
+        if(flag != 0) break;
+        else usleep(UDELAY);
     }
-    return fd;
+
+    return flag;
 }
 
- */
+JNIEXPORT jint JNICALL
+Java_org_blockinger_game_activities_ThreadForInput_setInput(JNIEnv *env, jobject thiz) {
+    int fd, flag;
+    unsigned char ret;
+    int value = 0;
+
+    fd = open("/dev/pushbutton", O_RDWR);
+    if(fd < 0) {
+        printf("Error opening device\n");
+    }
+
+    while(1) {
+        read(fd, &value, sizeof(int));
+        flag = translate(value);
+        if(flag != 0) break;
+        else usleep(UDELAY);
+    }
+
+    return flag;
+}
