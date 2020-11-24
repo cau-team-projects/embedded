afacb77d780aaa5b67a7aeab32cdcc44dd647a69
Lee Jae Hyeon <euphoric_n@naver.com>
2020-11-24 18:19:53 +0900

    remove duplicated motorControl function

--- app/src/main/cpp/motor.c
+++ app/src/main/cpp/motor.c
@@ -20,45 +20,6 @@
 #define OFF 0
 
 JNIEXPORT jint JNICALL
-Java_org_blockinger_game_components_GameState_motorControl(JNIEnv *env, jobject thiz, jint data, jint data2) {
-    // data = motor direction
-    // data2 = speed, 0(fast)~255(slow)
-    int fd;
-    int action, direction, speed;
-
-    fd = open("/dev/motor", O_RDWR);
-    if(fd < 0) {
-        printf("error opening device\n");
-        return -1;
-    }
-
-    switch(data) {
-        case CW :
-            action = 1;
-            direction = 1;
-            speed = data2;
-            ioctl(fd, MOTOR_SET_ACTION, &action, _IOC_SIZE(MOTOR_SET_ACTION));
-            ioctl(fd, MOTOR_SET_DIRECTION, &direction, _IOC_SIZE(MOTOR_SET_DIRECTION));
-            ioctl(fd, MOTOR_SET_SPEED, &speed, _IOC_SIZE(MOTOR_SET_SPEED));
-            break;
-        case CCW :
-            action = 1;
-            direction = 0;
-            speed = data2;
-            ioctl(fd, MOTOR_SET_ACTION, &action, _IOC_SIZE(MOTOR_SET_ACTION));
-            ioctl(fd, MOTOR_SET_DIRECTION, &direction, _IOC_SIZE(MOTOR_SET_DIRECTION));
-            ioctl(fd, MOTOR_SET_SPEED, &speed, _IOC_SIZE(MOTOR_SET_SPEED));
-            break;
-        case OFF :
-            action = 0;
-            ioctl(fd, MOTOR_SET_ACTION, &action, _IOC_SIZE(MOTOR_SET_ACTION));
-    }
-
-    close(fd);
-    return 0;
-}
-
-JNIEXPORT jint JNICALL
 Java_org_blockinger_game_components_MotorTimer_motorControl(JNIEnv *env, jobject thiz, jint time, jint data,
                                                             jint data2) {
     // data = motor direction
